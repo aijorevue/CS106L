475df13dcfdf711595927a385d799fce90821567
aijorevue <qinnovation013023@163.com>
2026-01-11 11:03:48 +0800

    add ostingstream::ate

--- the_Streams_one/stream.cpp
+++ the_Streams_one/stream.cpp
@@ -2,7 +2,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    ostringstream oss("Ito En Green Tea");
+    ostringstream oss("Ito En Green Tea",ostringstream::ate);
+    cout<<oss.str()<<endl;
+
+    oss<<16.9<<" Ounce";
     cout<<oss.str()<<endl;
     return 0;
 }
